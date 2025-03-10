e3e69f51ef88da9f4cf2657030664c0d17635578
kwasibaidoo <kwasi.baidoo@amalitech.com>
2025-03-10 11:06:42 +0000

    Updated Controller to RestController

--- src/main/java/com/week4ecr/week4ecr/controllers/HelloController.java
+++ src/main/java/com/week4ecr/week4ecr/controllers/HelloController.java
@@ -1,10 +1,10 @@
 package com.week4ecr.week4ecr.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class HelloController {
 
     @GetMapping("/")
